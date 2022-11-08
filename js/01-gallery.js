0c2136e87190cb37b0cc9d06eab893dd49f1c09a
max-ignat <cadillac.maxim@gmail.com>
2022-11-08 16:56:08 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,64 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-1
+
+const gallery = document.querySelector(".gallery");
+// console.log(createNewElement(galleryItems));
+
+gallery.insertAdjacentHTML("beforeend", createNewElement(galleryItems));
+
+function createNewElement(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+</div>`;
+        })
+        .join("");
+}
+
+
+
+
+gallery.addEventListener('click', openModal);
+
+
+function openModal(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+    return;
+    }
+
+    const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="800" height="600" alt=${event.target.alt}> `,
+    {
+    onShow: (instance) => {
+        window.addEventListener('keydown', escapePress);
+    },
+
+    onClose: (instance) => {
+        window.removeEventListener('keydown', escapePress);
+    },
+    }
+    );
+
+    
+    function escapePress(event) {
+    if (event.key !== 'Escape') {
+    return;
+    }
+    instance.close();
+    }
+
+    instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+// console.log(createNewElement(galleryItems));
+function createNewElement(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", createNewElement(galleryItems));
+
+
+const galleryEl = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
